f6df3a94ab0eb7c689e28fe5f41cdfdd33a92d81
Jeffrey Faray <lukonga05@gmail.com>
2025-08-18 10:47:41 +0300

    Rearranging the database name place in the code

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 
 import mongoose from "mongoose";
 
-const MONGODB_URI = `${process.env.MONGODB_URI}/swiftbuy`;
+// const MONGODB_URI = `${process.env.MONGODB_URI}/swiftbuy`;
 
 let cached = global.mongoose || { conn: null, promise: null };
 
@@ -11,15 +11,13 @@ export default async function connectDB() {
   if (!cached.promise) {
     const options = {
       bufferCommands: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/swiftbuy`, options).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
   global.mongoose = cached; // Keep cache in the global scope
 
-  return cached.conn;
+  return cached.conn; 
 }
